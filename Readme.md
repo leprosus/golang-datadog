db1c76d4a877a2299ae09382ee56368d54986d88
Denis Korolev <admin@leprosus.ru>
2017-10-16 19:28:41 +0200

    Add: package is realized

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Golang package to response service status for datadog service
+
+## Usage
+
+```go
+d := golang_datadog.NewDataDog(300) // Set status time-to-live - 300 seconds
+d.Handle("localhost", 8080, "/status") // Handle interface at localhost:8080 by /status http path (can use curl -X POST localhost:8080/status)
+d.SetStatus(status) // To re-new status
+// Before 300 seconds interface returns true & 200 status
+// After 300 seconds interface returns false & 204 status
+```
---
+++ datadog.go
@@ -0,0 +1,51 @@
+package golang_datadog
+
+import (
+	"net/http"
+	"sync"
+	"fmt"
+	"time"
+)
+
+type DataDog struct {
+	sync.Once
+
+	timestamp time.Time
+	ttl       uint64
+	status    bool
+}
+
+func NewDataDog(ttl uint64) *DataDog {
+	return &DataDog{
+		timestamp: time.Now(),
+		ttl:       ttl,
+		status:    false}
+}
+
+func (d *DataDog) Handle(host string, port uint64, router string) {
+	d.Once.Do(func() {
+		addr := fmt.Sprintf("%s:%d", host, port)
+
+		http.HandleFunc(router, func(w http.ResponseWriter, r *http.Request) {
+			if d.isOk() {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("true"))
+			} else {
+				w.WriteHeader(http.StatusNoContent)
+				w.Write([]byte("false"))
+			}
+		})
+
+		go http.ListenAndServe(addr, nil)
+	})
+}
+
+func (d *DataDog) isOk() bool {
+	return d.status &&
+		d.timestamp.Add(time.Duration(d.ttl) * time.Second).After(time.Now())
+}
+
+func (d *DataDog) SetStatus(status bool) {
+	d.status = status
+	d.timestamp = time.Now()
+}
